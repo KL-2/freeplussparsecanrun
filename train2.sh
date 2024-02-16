6539f92d92e2c7c423ce8e0bd780735a78585f26
KL-2 <3026187501@qq.com>
2024-02-16 08:25:37 +0800

    delete excess

--- train2.sh
+++
@@ -1,2 +0,0 @@
-srun -p priority --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 python train.py --gin_configs configs/freenerf/llff3_freenerf3.gin
-srun -p priority --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 python train.py --gin_configs configs/freenerf/llff3_freenerf4.gin
--- train3.sh
+++
@@ -1,3 +0,0 @@
-srun -p priority --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 python train.py --gin_configs configs/freenerf/llff3_freenerf5.gin
-srun -p priority --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 python train.py --gin_configs configs/freenerf/llff3_freenerf6.gin
-srun -p priority --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 python train.py --gin_configs configs/freenerf/llff3_freenerf7.gin
