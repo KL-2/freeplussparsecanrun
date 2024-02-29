6e03a41b2e91b3317911c06180071e0a060e2e87
KL-2 <3026187501@qq.com>
2024-02-29 10:42:08 +0800

    22222

--- scripts/train_and_eval_llff.sh
+++ scripts/train_and_eval_llff.sh
@@ -8,6 +8,7 @@ do
     echo "n_input_views is $i"
     CUDA_VISIBLE_DEVICES=0  python train_depth.py --gin_configs configs/llff3_freenerf_plus.gin --checkpoint_dir "llff3_"  --dataset_id "flower" --n_input_views "$i" --postfix "sparsity$i" # && \
 done
+
 my_array=(1 2 3 4 5 6 7 8 9 10 11 12 13 14)
 # 使用 for 循环遍历数组
 for i in "${my_array[@]}"
